ada455151c30d94432fc425a7781a747e36c1b6a
BfoxxyAlwayss <bfoxischaos@gmail.com>
2014-10-23 15:20:34 -0500

    BUildaHouse

--- BuildaHouse.py
+++ BuildaHouse.py
@@ -23,9 +23,30 @@ drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 
 # Insert your code here to draw the house!
-
-
+# Crating body of the house
 square = drawpad.create_rectangle(230,240,500,490, fill='Red')
+
+# Creating the roof
 line1 = drawpad.create_line(365,100,230,240)
 line2 = drawpad.create_line(365,100,500,240)
+
+# Door Creation
+rectangle = drawpad.create_rectangle(330,380,400,490, fill='brown')
+oval = drawpad.create_oval(335,430,345,440, fill='yellow')
+
+# Creating the windows
+square = drawpad.create_rectangle(250,260,320,330, fill='light blue')
+square = drawpad.create_rectangle(410,260,480,330, fill='light blue')
+
+#Chimney Creation
+line3 = drawpad.create_line(420,110,420,157)
+line4 = drawpad.create_line(420,110,460,110)
+line5 = drawpad.create_line(460,110,460,200)
+
+# Lawn
+rectangle = drawpad.create_rectangle(0,490,800,600, fill='green')
+
 root.mainloop()
+
+
+
